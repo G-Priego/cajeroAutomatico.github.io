90d3d4a9369414ba6f54a604770736709ebd5069
G-Priego <greta.priego.2000@gmail.com>
2023-12-23 11:02:54 -0600

    Cajero funcional

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function Bank() {
-    this.loginArea = document.getElementById('login-area');
-    this.recentLogins = document.getElementById('recent-logins');
-    this.dashboard = document.getElementById('dashboard');
+    this.loginArea = document.getElementById("login-area");
+    this.recentLogins = document.getElementById("recent-logins");
+    this.dashboard = document.getElementById("dashboard");
     this.userInfo = document.getElementById("message-area");
-    this.deposit = document.getElementById('deposit');
-    this.withdraw = document.getElementById('withdraw');
-    this.balance = document.getElementById('balance');
-    this.depositBox = document.getElementById('deposit-box');
-    this.withdrawBox = document.getElementById('withdraw-box');
-    this.depositInput = document.getElementById('deposit-input');
-    this.withdrawInput = document.getElementById('withdraw-input');
-    this.depositBtn = document.getElementById('deposit-btn');
-    this.withdrawBtn = document.getElementById('withdraw-btn');
-    this.submitBtn = document.getElementById('submit-btn');
-    this.logoutBtn = document.getElementById('logout-btn');
+    this.deposit = document.getElementById("deposit");
+    this.withdraw = document.getElementById("withdraw");
+    this.balance = document.getElementById("balance");
+    this.depositBox = document.getElementById("deposit-box");
+    this.withdrawBox = document.getElementById("withdraw-box");
+    this.depositInput = document.getElementById("deposit-input");
+    this.withdrawInput = document.getElementById("withdraw-input");
+    this.depositBtn = document.getElementById("deposit-btn");
+    this.withdrawBtn = document.getElementById("withdraw-btn");
+    this.submitBtn = document.getElementById("submit-btn");
+    this.logoutBtn = document.getElementById("logout-btn");
 
     this.bankAccounts = [
       { name: "Greta", balance: 200, PIN: 1234 },
@@ -21,16 +21,16 @@ function Bank() {
       { name: "Abigail", balance: 67, PIN: 6789 }
     ];
   
-    this.submitBtn.addEventListener('click', () => this.handleLogin());
-    this.depositBtn.addEventListener('click', () => this.handleDeposit());
-    this.withdrawBtn.addEventListener('click', () => this.handleWithdraw());
-    this.logoutBtn.addEventListener('click', () => this.handleLogout());
+    this.submitBtn.addEventListener("click", () => this.handleLogin());
+    this.depositBtn.addEventListener("click", () => this.handleDeposit());
+    this.withdrawBtn.addEventListener("click", () => this.handleWithdraw());
+    this.logoutBtn.addEventListener("click", () => this.handleLogout());
 }
 
 Bank.prototype.handleLogin = function () {
     
-    const inputName = document.getElementById('username-input').value;
-    const inputPIN = document.getElementById('pin-input').value;
+    const inputName = document.getElementById("username-input").value;
+    const inputPIN = document.getElementById("pin-input").value;
 
     const account = this.bankAccounts.find(
         account => account.name === inputName && account.PIN == inputPIN
@@ -67,7 +67,7 @@ Bank.prototype.handleDeposit = function () {
     }else if (value!=""){
         showErrors("Deposit");      
     }
-    this.depositInput.value = '';  
+    this.depositInput.value = "";  
     setTimeout(()=>{
         this.depositBox.classList.add("hidden");
     }, 6000)
@@ -86,7 +86,7 @@ Bank.prototype.handleWithdraw = function () {
     } else if (value!=""){
         showErrors("Withdraw");    
     }
-    this.withdrawInput.value = '';
+    this.withdrawInput.value = "";
     setTimeout(()=>{
         this.withdrawBox.classList.add("hidden");
     }, 6000)  
@@ -97,18 +97,18 @@ Bank.prototype.handleLogout = function () {
     this.recentLogins.style.display = "flex";
     this.dashboard.style.display = "none";
 
-    const inputName = document.getElementById('username-input').value = "";
-    const inputPIN = document.getElementById('pin-input').value = "";
+    const inputName = document.getElementById("username-input").value = "";
+    const inputPIN = document.getElementById("pin-input").value = "";
 };
 
 function showErrors(tipo){
     let error = document.getElementById(`error${tipo}`)
 
-    error.classList.remove('hidden')
-    error.classList.add('error')
+    error.classList.remove("hidden")
+    error.classList.add("error")
     setTimeout(()=>{
-        error.classList.remove('error')
-        error.classList.add('hidden')
+        error.classList.remove("error")
+        error.classList.add("hidden")
     }, 6000)   
 }
 
@@ -126,7 +126,7 @@ function getError(name, pin, array){
     }
     
     if(name == "" && pin == ""){
-        showErrors('Null')   
+        showErrors("Null")   
     } else if (nameFound==false) {
             showErrors("Name")
     } else if (pinFound==false) {
